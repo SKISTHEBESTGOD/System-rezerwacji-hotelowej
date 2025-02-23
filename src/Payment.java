23c9555101b5a72c1d014adaf9c7ca9f2515cf28
SKISTHEBESTGOD <skiperersiema@gmail.com>
2025-02-23 12:45:29 +0100

    nowa klasa Payment

---
+++ src/Payment.java
@@ -0,0 +1,22 @@
+public class Payment {
+    private double price;
+    private PaymentType paymentType;
+
+    public Payment(double price, PaymentType paymentType) {
+        this.price = price;
+        this.paymentType = paymentType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public PaymentTypegit  getPaymentType() {
+        return paymentType;
+    }
+
+    @Override
+    public String toString() {
+        return "Płatność w wysokości " + price + " przy użyciu " + paymentType;
+    }
+}
