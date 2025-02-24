e3fa0cec6d888fdd9a2707a050fba58aa3ab2aa8
SKISTHEBESTGOD <skiperersiema@gmail.com>
2025-02-24 17:04:47 +0100

    łatanie błędów

---
+++ src/User.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public abstract class User {
+    protected String firstName;
+    protected String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+    public abstract void displayMenu(Hotel hotel, Scanner scanner);
+}
